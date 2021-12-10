9cc45c683977c79a0b74077faa429ff8e8fbd1bf
bounderoll <bounderoll.23@gmail.com>
2021-12-10 23:46:31 +0400

    introduce express

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-import http from 'http'
+import express from 'express'
 
-const host = '127.0.0.1'
 const port = 3000
+const app = express()
 
-const server = http.createServer((req, res) => {
-    switch (req.method) {
-        case 'GET':
-            switch (req.url) {
-                case '/hello':
-                    res.statusCode = 200
-                    res.setHeader('Content-Type', 'text/plain')
-                    res.end('Hello !')
-                    break;
-            }
-            break;
-    }
+app.get('/hello', (req, res) => {
+    res.send('Hello !!!')
 })
 
-server.listen(port, host, () => {
-    console.log(`server activated in ${host}:${port}`)
+app.listen(port, () => {
+    console.log(`server listening on http://localhost:${port}`)
 })
