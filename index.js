5cfbe2a8039acba7a8a073447b1c5ab806423828
bounderoll <bounderoll.23@gmail.com>
2021-12-11 17:39:22 +0400

    add routing

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ import express from 'express'
 const port = 3000
 const app = express()
 
-app.get('/hello', (req, res) => {
-    res.send('Hello !!!')
+app.all('/hello', (req, res, next) => {
+    console.log(`all middleware`)
+    next()
 })
+
+const cb1 = (req, res, next) => {
+    console.log('callback 1')
+    next()
+}
+
+const cb2 = (req, res, next) => {
+    console.log('callback 2')
+    next()
+}
+
+app.route('/users')
+    .get(cb1, cb2, (req, res) => {
+        res.send('hello from get!')
+    })
+    .post(cb1, (req, res) => {
+        res.send('hello from post!')
+    })
 
 app.listen(port, () => {
     console.log(`server listening on http://localhost:${port}`)
