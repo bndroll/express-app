ff8a69942c4edb82019f5f46660b0450beb7be6c
bounderoll <bounderoll.23@gmail.com>
2021-12-12 19:22:26 +0400

    add basecontroller and userscontroller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Express } from 'express'
 import { Server } from 'http'
-import { userRouter } from './users/users'
+import { UsersController } from './users/users.controller'
 import { LoggerService } from './logger/logger.service'
 
 
@@ -9,15 +9,20 @@ export class App {
 	server: Server
 	port: number
 	logger: LoggerService
+	usersController: UsersController
 
-	constructor(logger: LoggerService) {
+	constructor(
+		logger: LoggerService,
+		usersController: UsersController
+	) {
 		this.app = express()
 		this.port = 3000
 		this.logger = logger
+		this.usersController = usersController
 	}
 
 	useRoutes() {
-		this.app.use('/users', userRouter)
+		this.app.use('/users', this.usersController.router)
 	}
 
 	public async init() {
