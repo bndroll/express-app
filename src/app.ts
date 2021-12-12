a1f0aa4c66793fb7da6d2006d06a766a46642f25
bounderoll <bounderoll.23@gmail.com>
2021-12-12 19:03:29 +0400

    logger service

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import express, { Express } from 'express'
 import { Server } from 'http'
 import { userRouter } from './users/users'
+import { LoggerService } from './logger/logger.service'
 
 
 export class App {
 	app: Express
 	server: Server
 	port: number
+	logger: LoggerService
 
-	constructor() {
+	constructor(logger: LoggerService) {
 		this.app = express()
 		this.port = 3000
+		this.logger = logger
 	}
 
 	useRoutes() {
@@ -20,6 +23,6 @@ export class App {
 	public async init() {
 		this.useRoutes()
 		this.server = this.app.listen(this.port)
-		console.log(`server listen on http://localhost:${this.port}`)
+		this.logger.log(`server listen on http://localhost:${this.port}`)
 	}
 }
